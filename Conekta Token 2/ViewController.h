cb83e8e512f8e00e43bc0d91b720246a4d0ab6d2
leonelgpr33 <leonel.perez@mrci.com.mx>
2015-12-31 18:28:28 -0600

    Initial Commit

---
+++ Conekta Token 2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Conekta Token 2
+//
+//  Created by LI Leonel G. Pérez Ramos on 31/12/15.
+//  Copyright © 2015 LI Leonel G. Pérez Ramos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Conekta Token 2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Conekta Token 2
+//
+//  Created by LI Leonel G. Pérez Ramos on 31/12/15.
+//  Copyright © 2015 LI Leonel G. Pérez Ramos. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
